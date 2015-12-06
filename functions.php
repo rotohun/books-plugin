646f9a7f4c44ddf6d2cf1b82deaa9866c605421b
Ronald Hunter <ronald@ronaldhunter.co>
2015-12-06 01:34:34 -0500

    First commit, Functions.php file for the books plugin.

---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+Plugin Name: Ronald Hunter
+Description: Description
+Plugin URI: http://ronaldhunter.co
+Author: Ronald Hunter
+Author URI: http://ronaldhunter.co
+Version: 1.0
+License: GPL2
+Text Domain: Text Domain
+Domain Path: Domain Path
+*/
+
+/*
+
+    Copyright (C) 2015  Ronald Hunter  ronald@ronaldhunter.co
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+
+?>

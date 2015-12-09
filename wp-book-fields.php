7e8920c548cff9ae1a06a961c6f28096626a0d07
Ronald Hunter <ronald@ronaldhunter.co>
2015-12-09 01:02:22 -0500

    added nonce to the file to make sure that the information beign stored was from the website

---
+++ wp-book-fields.php
@@ -0,0 +1,35 @@
+<?php
+
+function books_add_custom_metabox(){
+
+	add_meta_box(
+		'books meta box',
+		'Book',
+		'rth_books_meta_callback',
+		'slug'
+	 );
+
+}
+
+add_action('add_meta_boxes','books_add_custom_metabox');
+
+
+function rth_books_meta_callback($post){
+	wp_nonce_field(basename(__FILE__), 'rth_books_nonce' );
+	$rth_books_stored_meta = get_post_meta($post->ID);
+	
+	?>
+
+		<div>
+			<div class="meta-row">
+				<div class="meta-head">
+					<label for="book-id" class="row-title">Book Name</label>
+				</div>
+				<div class="meta-cell">
+					<input type="text" name"book-id" id="book-id" value=""/>
+				</div>
+			</div>
+		</div>
+
+	<?php
+}

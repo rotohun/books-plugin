59ec89af9ef53c542a2b7de15e2f4290dab5fe93
Ronald Hunter <ronald@ronaldhunter.co>
2015-12-09 01:27:07 -0500

    added another label and input field

--- wp-book-fields.php
+++ wp-book-fields.php
@@ -17,7 +17,7 @@ add_action('add_meta_boxes','books_add_custom_metabox');
 function rth_books_meta_callback($post){
 	wp_nonce_field(basename(__FILE__), 'rth_books_nonce' );
 	$rth_books_stored_meta = get_post_meta($post->ID);
-	
+
 	?>
 
 		<div>
@@ -27,6 +27,12 @@ function rth_books_meta_callback($post){
 				</div>
 				<div class="meta-cell">
 					<input type="text" name"book-id" id="book-id" value=""/>
+					<div class="meta-head">
+					<label for="date-made" class="row-title">Date Made</label>
+				</div>
+				<div class="meta-cell">
+					<input type="text" name"date-made" id="date-made" value=""/>
+				</div>
 				</div>
 			</div>
 		</div>

8fb52831053b3511e873cb4bf6dee9c280a83159
Ronald Hunter <ronald@ronaldhunter.co>
2015-12-09 00:36:20 -0500

    Added two filed to hold the plugin meta boxes and to contain the plugins file routes

--- functions.php
+++ functions.php
@@ -31,71 +31,22 @@ Domain Path: Domain Path
 */
 
         
-        
-function Wp_alter_books_icon($args)
-{
-	$args['menu_icons'] = 'dashicons-book-alt';
-	return $args;
-}
-
-add_filter('Wp_post_type_args',  'Wp_alter_books_icon' ); 
-
-function registar_post_type_books()
-{
-	
-		$labels = array(
-			'name'                => __( 'Books', 'text-domain' ),
-			'singular_name'       => __( 'Book', 'text-domain' ),
-			'add_new'             => _x( 'Add New Book', 'text-domain', 'text-domain' ),
-			'add_new_item'        => __( 'Add New Book', 'text-domain' ),
-			'edit_item'           => __( 'Edit book', 'text-domain' ),
-			'new_item'            => __( 'New book', 'text-domain' ),
-			'view_item'           => __( 'View book', 'text-domain' ),
-			'search_items'        => __( 'Search Plural Name', 'text-domain' ),
-			'not_found'           => __( 'No Plural Name found', 'text-domain' ),
-			'not_found_in_trash'  => __( 'No Plural Name found in Trash', 'text-domain' ),
-			'parent_item_colon'   => __( 'Parent Book', 'text-domain' ),
-			'menu_name'           => __( 'Books', 'text-domain' ),
-		);
-	
-		$args = array(
-			'labels'                   => $labels,
-			'hierarchical'        => false,
-			'description'         => 'description',
-			'taxonomies'          => array(),
-			'public'              => true,
-			'show_ui'             => true,
-			'show_in_menu'        => true,
-			'show_in_admin_bar'   => true,
-			'menu_position'       => null,
-			'menu_icon'           => 'dashicons-book-alt',
-			'show_in_nav_menus'   => true,
-			'publicly_queryable'  => true,
-			'exclude_from_search' => false,
-			'has_archive'         => true,
-			'query_var'           => true,
-			'can_export'          => true,
-			'rewrite'             => true,
-			'capability_type'     => 'post',
-			'supports'            => array(
-				'title', 'editor', 'author', 'thumbnail',
-				'excerpt','custom-fields', 'comments',
-				'revisions', 'page-attributes', 'post-formats'
-				)
-		);
-	
-		register_post_type( 'slug', $args );
-}
-	
-add_action( 'after_setup_theme', 'registar_post_type_books' );
+  if (! defined('ABSPATH')){
+  	 exit;
+  }    
 
 
-function registar_taxonomy_books(){
+ $dir = plugin_dir_path(__FILE__);
+
+  require_once( $dir . 'wp-book-cpt.php');
+  require_once ($dir . 'wp-book-fields.php');
 
 
 
-	
-}
 
 
-?>
+
+
+
+
+
